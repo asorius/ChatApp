887410620dde7b9111ea10ab72338d297a54dddc
ddexta <18assdd@gmail.com>
2018-07-29 15:01:35 +0100

    set up basic heroku server requirements

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const path=require('path')
 const publicPath=path.join(__dirname,'../public')
 const express=require('express')
 
+// for heroku
+const port=process.env.PORT || 3000
+
 const app=express()
 app.use(express.static(publicPath))
 
 
-
-app.listen(3000,()=>{
-    console.log('Server up on port 3000...')
+app.listen(port,()=>{
+    console.log(`Server up on port ${port}...`)
 })
 
